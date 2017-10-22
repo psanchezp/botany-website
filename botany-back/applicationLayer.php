d744ff1135c8247c3da2f3d1259a8e7549c0e29f
Andres Altamirano <andy_altamirano95@hotmail.com>
2017-10-22 18:20:16 -0500

    added error status 434

--- botany-back/applicationLayer.php
+++ botany-back/applicationLayer.php
@@ -558,6 +558,9 @@
             case "433" : header("HTTP/1.1 411 No se ha podido registrar la venta.");
                         die("No se ha podido registrar la venta con el cliente.");
                         break;
+            case "434" : header("HTTP/1.1 411 No se ha podido registrar la compra.");
+                        die("No se ha podido registrar la compra con el proveedor.");
+                        break;
 			case "ERROR" : header('HTTP/1.1 416 No existe un usuario registrado con los datos dados.');
                         die("Los datos ingresados no coinciden con un usuario registrado.");
 						break;
