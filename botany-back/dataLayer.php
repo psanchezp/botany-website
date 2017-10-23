7d4615ac0760feb9edb53e6ef8e29221acee7759
Andres Altamirano <andy_altamirano95@hotmail.com>
2017-10-22 19:17:24 -0500

    implemented attemptUpdatePurchase function

--- botany-back/dataLayer.php
+++ botany-back/dataLayer.php
@@ -533,13 +533,34 @@
             $result = SQLUpdateFullSale($connection, $saleID, $clientUsername, SQLGetProductIDFromName($connection, $productName), $transactionDate, $state, $quantity, $description);
 
             if ($result) {
-                $response = array("status" => "SUCCESS", "saleID" => $saleID, "username" => $clientUsername, "productName" => $productName, "transactionDate" => $transactionDate, "description" => $description, "state" => $state, "quantity" => $quantity, "transactionType" => "sale");
+                $response = array("status" => "SUCCESS", "id" => $saleID, "username" => $clientUsername, "productName" => $productName, "transactionDate" => $transactionDate, "description" => $description, "state" => $state, "quantity" => $quantity, "transactionType" => "sale");
 
                 $connection->close();
                 return $response;
             } else {
                 $connection->close();
                 return array("status" => "435");
+            }
+        } else {
+            return array("status" => "500");
+        }
+    }
+
+    function attemptUpdatePurchase($purchaseID, $providerUsername, $productName, $transactionDate, $state, $quantity, $description) {
+        $connection = connectionToDataBase();
+
+        if ($connection != null) {
+
+            $result = SQLUpdateFullPurchase($connection, $purchaseID, $providerUsername, SQLGetProductIDFromName($connection, $productName), $transactionDate, $state, $quantity, $description);
+
+            if ($result) {
+                $response = array("status" => "SUCCESS", "id" => $purchaseID, "username" => $providerUsername, "productName" => $productName, "transactionDate" => $transactionDate, "description" => $description, "state" => $state, "quantity" => $quantity, "transactionType" => "purchase");
+
+                $connection->close();
+                return $response;
+            } else {
+                $connection->close();
+                return array("status" => "436");
             }
         } else {
             return array("status" => "500");
