786de94aea6df43684159c59218a74390e1d7ce2
psanchezp <psanchezp94@gmail.com>
2017-10-26 18:02:14 -0500

    Added provider create, show, index, delete

---
+++ botany-front/src/components/show/ProviderShow.vue
@@ -0,0 +1,81 @@
+<template>
+  <md-table class="provider-show" v-once>
+    <md-table-header>
+      <md-table-row>
+        <md-table-head>Usuario</md-table-head>
+        <md-table-head>Nombre</md-table-head>
+        <md-table-head>Descripción</md-table-head>
+        <md-table-head>Telefono</md-table-head>
+        <md-table-head>Address</md-table-head>
+        <md-table-head>Correo</md-table-head>
+        <md-table-head>Editar</md-table-head>
+        <md-table-head>Borrar</md-table-head>
+      </md-table-row>
+    </md-table-header>
+    <md-table-body>
+      <md-table-row>
+        <md-table-cell>
+          {{ providerItem.username }}
+        </md-table-cell>
+        <md-table-cell>
+          {{ providerItem.name }}
+        </md-table-cell>
+        <md-table-cell>
+          {{ providerItem.description }}
+        </md-table-cell>
+        <md-table-cell>
+          {{ providerItem.phone }}
+        </md-table-cell>
+        <md-table-cell>
+          {{ providerItem.address }}
+        </md-table-cell>
+        <md-table-cell>
+          {{ providerItem.email }}
+        </md-table-cell>
+        <md-table-cell>
+          <edit-button
+            model="products"
+            :name="providerItem.name"
+          >
+          </edit-button>
+        </md-table-cell>
+        <md-table-cell>
+          <delete-button
+            :variable="variable"
+            :name="providerItem.name"
+            :action="action"
+          >
+          </delete-button>
+        </md-table-cell>
+      </md-table-row>
+    </md-table-body>
+  </md-table>
+</template>
+
+<script>
+import DeleteButton from '@/components/buttons/DeleteButton'
+import EditButton from '@/components/buttons/EditButton'
+
+export default {
+  name: 'provider-show',
+  data () {
+    return {
+      variable: 'productName',
+      action: 'DELETE_PRODUCT'
+    }
+  },
+  components: {
+    DeleteButton, EditButton
+  },
+  props: {
+    providerItem: Object
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .md-table {
+    width: 100%;
+  }
+</style>
--- botany-front/src/components/tables/ProviderTable.vue
+++ botany-front/src/components/tables/ProviderTable.vue
@@ -1,49 +1,84 @@
 <template>
-  <div class="client-table">
-    <md-layout md-gutter>
-      <md-layout md-flex="70" md-flex-offset="15">
-        <md-table v-once>
-          <md-table-header>
-            <md-table-row>
-              <md-table-head>Nombre</md-table-head>
-              <md-table-head>Correo</md-table-head>
-              <md-table-head>Telefono</md-table-head>
-              <md-table-head>Editar</md-table-head>
-              <md-table-head>Borrar</md-table-head>
-            </md-table-row>
-          </md-table-header>
+  <md-table class="provider-table" v-once>
+    <md-table-header>
+      <md-table-row>
+        <md-table-head>Usuario</md-table-head>
+        <md-table-head>Nombre</md-table-head>
+        <md-table-head>Descripción</md-table-head>
+        <md-table-head>Telefono</md-table-head>
+        <md-table-head>Dirección</md-table-head>
+        <md-table-head>Correo</md-table-head>
+        <md-table-head>Editar</md-table-head>
+        <md-table-head>Borrar</md-table-head>
+        <md-table-head>Información</md-table-head>
+      </md-table-row>
+    </md-table-header>
 
-          <md-table-body>
-            <md-table-row v-for="(row, index) in 3" :key="index">
-              <md-table-cell>
-                <a class="md-transparent" href="/product/x">
-                  Juanito
-                </a>
-              </md-table-cell>
-              <md-table-cell>juan@juan.com</md-table-cell>
-              <md-table-cell>8115433345</md-table-cell>
-              <md-table-cell><edit-button></edit-button></md-table-cell>
-              <md-table-cell><delete-button></delete-button></md-table-cell>
-            </md-table-row>
-          </md-table-body>
-        </md-table>
-      </md-layout>
-    </md-layout>
-  </div>
+    <md-table-body>
+      <md-table-row v-for="item of providerItems" :key="item.name">
+        <md-table-cell>
+          {{ item.username }}
+        </md-table-cell>
+        <md-table-cell>
+          {{ item.name }}
+        </md-table-cell>
+        <md-table-cell>
+          {{ item.description }}
+        </md-table-cell>
+        <md-table-cell>
+          {{ item.phone }}
+        </md-table-cell>
+        <md-table-cell>
+          {{ item.address }}
+        </md-table-cell>
+        <md-table-cell>
+          {{ item.email }}
+        </md-table-cell>
+        <md-table-cell>
+          <edit-button
+            model="providers"
+            :name="item.name"
+          >
+          </edit-button>
+        </md-table-cell>
+        <md-table-cell>
+          <delete-button
+            :variable="variable"
+            :name="item.name"
+            :action="action"
+          >  
+          </delete-button>
+        </md-table-cell>
+        <md-table-cell>
+          <info-button
+            model="providers"
+            :name="item.username"
+          >
+          </info-button>
+        </md-table-cell>
+      </md-table-row>
+    </md-table-body>
+  </md-table>
 </template>
 
 <script>
 import DeleteButton from '@/components/buttons/DeleteButton'
 import EditButton from '@/components/buttons/EditButton'
+import InfoButton from '@/components/buttons/InfoButton'
 
 export default {
-  name: 'client-table',
+  name: 'provider-table',
   data () {
     return {
+      variable: 'productName',
+      action: 'DELETE_PROVIDER'
     }
   },
   components: {
-    DeleteButton, EditButton
+    DeleteButton, EditButton, InfoButton
+  },
+  props: {
+    providerItems: Array
   }
 }
 </script>
