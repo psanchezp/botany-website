323e694fe1b5ca283fb317d8cbaaafff8b8be357
psanchezp <psanchezp94@gmail.com>
2017-11-14 23:46:21 -0600

    Report views

---
+++ botany-front/src/components/Report.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="report-stuff">
+    <nav-bar></nav-bar>
+    <h1>Generar reporte</h1>
+    <report-index></report-index>
+  </div>
+</template>
+
+<script>
+  import ReportIndex from '@/components/ReportIndex'
+  import NavBar from '@/components/NavBar'
+
+  export default {
+    name: 'report-stuff',
+    data () {
+      return {
+      }
+    },
+    components: {
+      NavBar, ReportIndex
+    }
+  }
+</script>
