a14c3caae78c745edff3d3907021fde11910ebc7
psanchezp <psanchezp94@gmail.com>
2017-11-14 23:52:19 -0600

    Transaction tables updated

--- botany-front/src/components/TransactionIndex.vue
+++ botany-front/src/components/TransactionIndex.vue
@@ -10,17 +10,35 @@
   import TransactionAdmin from '@/components/TransactionAdmin'
   import TransactionUser from '@/components/TransactionUser'
   import NavBar from '@/components/NavBar'
+  import axios from 'axios'
 
   export default {
     name: 'transaction-index',
     data () {
       return {
-        model: this.$route.params.model,
-        name: this.$route.params.name
+        model: '',
+        name: this.$route.params.name,
+        url: 'http://127.0.0.1/botany-back/applicationLayer.php'
       }
     },
     components: {
       TransactionAdmin, TransactionUser, NavBar
+    },
+    created: function () {
+      this.checkUser()
+    },
+    methods: {
+      checkUser () {
+        var params = new URLSearchParams()
+        params.append('action', 'GET_CURRENT_USER')
+        axios.post(this.url, params)
+          .then(function (response) {
+            this.model = response.data.type
+          }.bind(this))
+          .catch(function (error) {
+            console.log(error)
+          })
+      }
     }
   }
 </script>
